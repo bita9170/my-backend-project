7e34cbc7fc9c443e92d9b25ef3e1cafe2d7c3330
bita9170 <bita.s9170@gmail.com>
2024-04-16 11:53:01 +0200

    [pl-12]  Optimization api handling

--- app/api/polls/route.ts
+++ app/api/polls/route.ts
@@ -3,41 +3,22 @@ import { NextRequest, NextResponse } from "next/server";
 import Poll from "@/libs/model/Poll";
 
 export async function GET() {
-  await connectMongoDB();
-  const allPolls = await Poll.find();
-  return NextResponse.json({ polls: allPolls }, { status: 200 });
+  try {
+    await connectMongoDB();
+    const allPolls = await Poll.find();
+    return NextResponse.json({ polls: allPolls }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 400 });
+  }
 }
 
 export async function POST(req: NextRequest) {
-  const { question, options } = await req.json();
-  await connectMongoDB();
-  await Poll.create({ question, options });
-  return NextResponse.json({ message: "Poll Created" }, { status: 201 });
-}
-
-export async function PUT(req: NextRequest) {
-  const { id, question, options } = await req.json();
-  await connectMongoDB();
-  const updatedPoll = await Poll.findByIdAndUpdate(
-    id,
-    { question, options },
-    { new: true }
-  );
-  if (!updatedPoll) {
-    return NextResponse.json({ message: "Poll not found" }, { status: 404 });
+  try {
+    const { question, options } = await req.json();
+    await connectMongoDB();
+    await Poll.create({ question, options });
+    return NextResponse.json({ message: "Poll Created" }, { status: 201 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 201 });
   }
-  return NextResponse.json(
-    { message: "Poll updated", poll: updatedPoll },
-    { status: 200 }
-  );
-}
-
-export async function DELETE(req: NextRequest) {
-  const { id } = await req.json();
-  await connectMongoDB();
-  const deletedPoll = await Poll.findByIdAndDelete(id);
-  if (!deletedPoll) {
-    return NextResponse.json({ message: "Poll not found" }, { status: 404 });
-  }
-  return NextResponse.json({ message: "Poll deleted" }, { status: 200 });
 }
